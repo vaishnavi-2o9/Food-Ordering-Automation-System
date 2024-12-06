1a35e5b264c840fe832c819cbd311f009bf2e493
vaishnavich00 <vaishnavic772@gmail.com>
2024-12-07 01:32:21 +0530

    add cart.js

---
+++ menu.js
@@ -0,0 +1,27 @@
+function openNewWindow() {
+    // Aapke requirement ke hisaab se nayi window/URL open karna
+    const urlMapping = {
+        "burger.jpeg": "burgerDetails.html",
+        "cheeseburger.jpeg": "cheeseBurgerDetails.html",
+        "veggie burger.webp": "veggieBurgerDetails.html",
+        "chilli cheese burger.jpg": "chilliCheeseBurgerDetails.html",
+        "avocado bacon burger.jpg": "avocadoBaconBurgerDetails.html",
+        "bbq bacon burger.jpeg": "bbqBaconBurgerDetails.html",
+        "mushroom swiss burger.jpg": "mushroomSwissBurgerDetails.html",
+        "burger.webp": "oldFashionBurgerDetails.html",
+        "double cheeseburger.jpeg": "doubleCheeseBurgerDetails.html"
+    };
+
+    // Image element jo click hua hai, use event ke through identify karna
+    const clickedImage = event.target.src.split('/').pop(); // image ka naam extract karna
+
+    // URL mapping se matching URL lena
+    const redirectUrl = urlMapping[clickedImage];
+
+    // Agar URL mile, toh us page par redirect karein
+    if (redirectUrl) {
+        window.location.href = redirectUrl;
+    } else {
+        console.error("No URL mapped for this image.");
+    }
+}
