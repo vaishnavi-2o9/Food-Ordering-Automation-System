740620cfb668808fec857ec9b9473a5c610bc9cc
vaishnavich00 <vaishnavic772@gmail.com>
2024-12-28 20:42:35 +0530

    cart

--- cart.js
+++ cart.js
@@ -1,24 +1,40 @@
-let cart = {};
+let cart = {}
+function addToCart(productName) {
+    if (cart[productName]) {
+        cart[productName]++;
+    } else {
+        cart[productName] = 1;
+    }
+    updateCartCount();
+}
 
-        function addToCart(productName) {
-            if (cart[productName]) {
-                cart[productName]++;
-            } else {
-                cart[productName] = 1;
-            }
-            updateCartCount();
+function updateCartCount() {
+    let count = Object.keys(cart).length;
+    document.getElementById('cart-count').innerText = count;
+}
+
+function showCart() {
+    // Save cart data to localStorage
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    // Redirect to a new page to display the cart details
+    window.location.href = 'cart-details.html';
+}
+
+
+window.onload = function() {
+    let cart = JSON.parse(localStorage.getItem('cart')) || {};
+
+    if (Object.keys(cart).length === 0) {
+        // If cart is empty
+        document.getElementById('empty-cart').style.display = 'block';
+    } else {
+        // Display cart items
+        let cartItemsList = document.getElementById('cart-items');
+        for (let product in cart) {
+            let listItem = document.createElement('li');
+            listItem.innerText = `${product}: ${cart[product]}`;
+            cartItemsList.appendChild(listItem);
         }
-
-        function updateCartCount() {
-            let count = Object.keys(cart).length;
-            document.getElementById('cart-count').innerText = count;
-        }
-
-        function showCart() {
-            let cartItems = '';
-            for (let item in cart) {
-                cartItems += `${item}: ${cart[item]}\n`;
-            }
-            alert('Products in cart:\n' + cartItems);
-        }
-
+    }
+};
