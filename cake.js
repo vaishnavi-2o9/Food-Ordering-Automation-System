dcbc1001b1e460c6fd6ec98ee73105df63c5b956
vaishnavi Shukla <vaishnavi29oo5@gmail.com>
2025-01-08 12:13:19 +0530

    Changes

---
+++ cake.js
@@ -0,0 +1,4 @@
+function openNewWindow(name, price) {
+    // Open cprize.html in a new window with item details as URL parameters
+    window.open(`cprize.html?name=${encodeURIComponent(name)}&price=${price}`, '_blank', 'width=600,height=400');
+}
