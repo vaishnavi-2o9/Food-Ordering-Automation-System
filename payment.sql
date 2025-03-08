fe906e55300b88512c483ec87af36d883e81b309
vaishnavich00 <vaishnavic772@gmail.com>
2025-03-08 12:41:29 +0530

    changes

--- payment.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE customer;
-USE customer;
-
-CREATE TABLE payments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    transaction_id VARCHAR(100) UNIQUE,
-    payment_status VARCHAR(50) DEFAULT 'Pending',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
